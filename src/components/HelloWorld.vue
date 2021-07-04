d71836098c951e2073d6b33d2c82666938e8b840
Sourav Bera <souravbera472@.com>
2021-07-04 18:47:07 +0530

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,46 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <br />
+    <v-row class="row1">
+      <h1>ABC High School</h1>
+      <v-spacer></v-spacer>
+       <v-btn class="text">
+         courses
+       </v-btn>
+
+        <v-icon class="btn"
+        large>mdi-format-list-bulleted</v-icon>
+    </v-row>
+    <img class="img" width="100%" :src="'image/1st.PNG'" alt />
+     <img class="img2" width="100%" :src="'image/2nd.PNG'" alt />
+     <img class="img2" width="100%" :src="'image/3rd.PNG'" alt />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.row1 {
+  background-color: rgb(24, 24, 82);
+  margin-left: 1px;
+  height: 100px !important;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+h1 {
+  margin-top: 2%;
+  color: white;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.text{
+  margin-top: 2.5%;
+  border: 6px solid rgb(32, 165, 226);
+  
 }
-a {
-  color: #42b983;
+.btn{
+  margin-left: 2%;
+  color:white !important;
+  margin-right: 3%;
+  cursor: pointer;
 }
 </style>
